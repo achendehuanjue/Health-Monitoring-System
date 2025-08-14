3352b4d1ead962b09a3b1c6c370d5d3f4933a634
THE CHEN <1969334906@qq.com>
2025-08-14 21:27:35 +0800

    Delete demo-srv directory

--- demo-srv/handler/model/device_type.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gorm.io/gorm"
-
-type DeviceType struct { //设备类型
-	gorm.Model
-	Name string `gorm:"type:varchar(50);not null;index;comment:设备类型名称"`
-	Desc string `gorm:"type:varchar(255);not null;index;comment:设备类型描述"`
-}
