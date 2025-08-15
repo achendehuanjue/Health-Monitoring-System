7ba123373c1be9102a14cf03990636b5f9a5a02c
孙亿龙 <15504168+son-of-the-dragon@user.noreply.gitee.com>
2025-08-15 22:02:13 +0800

    添加清洗规则

--- pex-srv/handler/dao/comment.go
+++ pex-srv/handler/dao/comment.go
@@ -9,6 +9,7 @@ import (
 	"time"
 )
 
+// 体温
 func TemperatureEmqxClient() {
 	clientID := fmt.Sprintf("mqtt-consumer-Temperature-%d", time.Now().UnixNano())
 
@@ -22,6 +23,7 @@ func TemperatureEmqxClient() {
 
 }
 
+// 血糖
 func BloodGlucoseEmqxClient() {
 	clientID := fmt.Sprintf("mqtt-consumer-BloodGlucose-%d", time.Now().UnixNano())
 
@@ -34,6 +36,7 @@ func BloodGlucoseEmqxClient() {
 	pkg.MqttClient(config.BloodGlucose)
 }
 
+// 血压
 func BloodPressureEmqxClient() {
 	clientID := fmt.Sprintf("mqtt-consumer-BloodPressure-%d", time.Now().UnixNano())
 
@@ -46,6 +49,7 @@ func BloodPressureEmqxClient() {
 	pkg.MqttClient(config.BloodPressure)
 }
 
+// 血氧
 func BloodOxygenEmqxClient() {
 	clientID := fmt.Sprintf("mqtt-consumer-BloodOxygen-%d", time.Now().UnixNano())
 
